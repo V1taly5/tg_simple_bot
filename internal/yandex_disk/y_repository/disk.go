b009e3b6b04e6df861e55b4fd0f2cb14a9d705cd
V1taly5 <egorov21112061vito@gmail.com>
2024-03-15 00:17:52 +0300

    added methods for working with Yandex disk

--- internal/yandex_disk/y_repository/disk.go
+++ internal/yandex_disk/y_repository/disk.go
@@ -21,3 +21,64 @@ func NewYandexDisk(client *http.Client, token *Token, baseUrl string) (*YandexDi
 	}
 	return &YandexDisk{Token: token, Client: NewClient}, nil
 }
+
+func (d *YandexDisk) GetDisk(ctx context.Context, fields []string) (*Disk, error) {
+	const op = "ydisk/GetDisk"
+	//add checking for zero
+	values := url.Values{}
+	values.Add("fields", strings.Join(fields, ","))
+
+	req, err := d.Client.MakeRequest(http.MethodGet, "/disk?"+values.Encode(), nil)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	disk := new(Disk)
+
+	_, err = d.Client.GetResponse(ctx, req, &disk)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return disk, nil
+}
+
+func (d *YandexDisk) GetResourceUploadLink(ctx context.Context, path string, fields []string, overwrite bool) (*ResourceUploadLink, error) {
+	const op = "ydisk/GetResourceUploadLink"
+
+	values := url.Values{}
+	values.Add("path", path)
+	values.Add("fields", strings.Join(fields, ","))
+	values.Add("overwrite", strconv.FormatBool(overwrite))
+
+	req, err := d.Client.MakeRequest(http.MethodGet, "/disk/resources/upload?"+values.Encode(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	resourceUploadLink := new(ResourceUploadLink)
+
+	_, err = d.Client.GetResponse(ctx, req, &resourceUploadLink)
+
+	return resourceUploadLink, nil
+}
+
+func (d *YandexDisk) UploudExternalResource(ctx context.Context, path string, externalUrl string, disableRedirects bool, fields []string) (*Link, error) {
+	const op = "ydisk/UploudExternalResource"
+
+	values := url.Values{}
+	values.Add("path", path)
+	values.Add("externalUrl", externalUrl)
+	values.Add("disableRedirects", strconv.FormatBool(disableRedirects))
+	values.Add("fields", strings.Join(fields, ","))
+
+	req, err := d.Client.MakeRequest(http.MethodPost, "/disk/resources/upload?"+values.Encode(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	link := new(Link)
+	_, err = d.Client.GetResponse(ctx, req, &link)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return link, nil
+}
